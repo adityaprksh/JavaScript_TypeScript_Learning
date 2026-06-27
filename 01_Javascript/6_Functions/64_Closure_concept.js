672bae7ea9bd215a76e73f334d792a094ec9ab13
adityaprksh <aditya18bwn@gmail.com>
2026-06-27 15:20:24 +0530

    check the git setup

--- 01_Javascript/6_Functions/64_Closure_concept.js
+++ 01_Javascript/6_Functions/64_Closure_concept.js
@@ -4,8 +4,28 @@ function outer(){
     function inner(){
         console.log(message," From Inner")
     }
-    return inner()
+    return inner
 }
 
 let fn_inner = outer()
 fn_inner()
+
+
+console.log("____________________________")
+
+ function makeCounter (start =0){
+    let count = start
+    return{
+        increment() {count++},
+        decrement() {count--},
+        get() {return count}
+    }
+ }
+
+ let counter = makeCounter()
+ counter.increment()
+ counter.increment()
+ counter.increment()
+console.log(counter.get())
+counter.decrement()
+console.log(counter.get()) 
